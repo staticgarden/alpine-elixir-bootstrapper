79869eb4a89d616e34ea6391a5872ce005669357
Khaja Minhajuddin <minhajuddink@gmail.com>
2018-06-23 18:11:20 +0530

    Fix paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,14 @@
 #   2. unzipping it
 #   3. running it
 
-echo "# downloading release: ${APP_NAME}/${RELEASE_TAR_PATH}"
-aws s3 cp \
-  "s3://${S3_BUCKET}/${RELEASE_TAR_PATH}" \
-  /tmp/
+RELEASE_TAR_FILENAME="$(basename "$RELEASE_TAR_PATH")"
+S3_PATH="s3://${S3_BUCKET}/${RELEASE_TAR_PATH}"
+
+echo "# downloading release: ${S3_PATH}"
+aws s3 cp "${S3_PATH}" /tmp/
 
 echo "# unzipping the release"
-tar xvzf /tmp/${RELEASE_TAR_PATH}
+tar xvzf /tmp/${RELEASE_TAR_FILENAME}
 
 echo "# starting the release"
 bin/${APP_NAME} foreground
